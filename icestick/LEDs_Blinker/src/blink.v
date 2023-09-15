47d65db91eb353d1fd0de5c21575cc66315508ee
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-15 21:45:46 +0400

    8 bit

--- icestick/LEDs_Blinker/src/blink.v
+++ icestick/LEDs_Blinker/src/blink.v
@@ -5,10 +5,18 @@ module blink
   output LED2,
   output LED3,
   output LED4,
-  output LED5
+  output LED5,
+  output PMOD0,
+  output PMOD1,
+  output PMOD2,
+  output PMOD3,
+  output PMOD4,
+  output PMOD5,
+  output PMOD6,
+  output PMOD7
 );
 
-  localparam bits = 5;
+  localparam bits = 8;
   localparam delay = 22;
 
   reg [ bits + delay-1 :0 ] counter = 0;
@@ -20,4 +28,5 @@ module blink
   end
 
   assign { LED1, LED2, LED3, LED4, LED5 } = out;
+  assign { PMOD0, PMOD1, PMOD2, PMOD3, PMOD4, PMOD5, PMOD6, PMOD7 } = out;
 endmodule
