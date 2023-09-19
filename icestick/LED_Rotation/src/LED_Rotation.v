c0b599669c34d92c236150c812d6b0df00129396
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-19 19:50:54 +0400

    rotation done

--- icestick/LED_Rotation/src/LED_Rotation.v
+++ icestick/LED_Rotation/src/LED_Rotation.v
@@ -4,43 +4,30 @@
 // (c) Sept 2023
 
 module LED_Rotation(
-    input  clk,
+    
+	input  clk,
+
     output LED1,
     output LED2,
     output LED3,
     output LED4,
     output LED5
-    );
+    
+	);
 
-	reg[15:0] div_cntr1;
-	reg[6:0] div_cntr2;
-	reg[1:0] dec_cntr;
-	reg half_sec_pulse;
-		
+	reg[19:0] 	div;		// Clock divider (2^20 bits)
+	reg[1:0] 	count;		// LED counter (0-3 = 2^2 bits)
+			
 	always@(posedge clk)
 		begin
-		div_cntr1 <= div_cntr1 + 1;
-		if (div_cntr1 == 0) 
-			if (div_cntr2 == 91) 
-				begin
-				div_cntr2 <= 0;
-				half_sec_pulse <= 1;  
-				end
-			else
-				div_cntr2 <= div_cntr2 + 1;
-		else
-			half_sec_pulse <= 0;
-		
-		if (half_sec_pulse == 1)	
-			dec_cntr <= dec_cntr + 1;
-			
+			div <= div + 1;
+			if (div == 0) count <= count + 1;
 		end	
-		
-		
-	assign LED1 = (dec_cntr == 0) ;
-	assign LED2 = (dec_cntr == 1) ;
-	assign LED3 = (dec_cntr == 2) ;
-	assign LED4 = (dec_cntr == 3) ;
+				
+	assign LED1 = (count == 0);
+	assign LED2 = (count == 1);
+	assign LED3 = (count == 2);
+	assign LED4 = (count == 3);
 	assign LED5 = 1'b1;
 				
 endmodule
