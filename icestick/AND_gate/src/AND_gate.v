17b884ed28768e62729ce405c6d18949da881a7e
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-20 21:45:08 +0400

    WIP

--- icestick/AND_gate/src/AND_gate.v
+++ icestick/AND_gate/src/AND_gate.v
@@ -1,31 +1,10 @@
-
-  
 // AND gate example
-//
-// Inputs:
-//      pmod_0  - pushbutton
-//      pmod_1  - pushbutton
-// Outputs:
-//      led_0   - LED
-//
-// LED will turn on if both pushbuttons are pressed (~pmod_0 & ~pmod_1).
-//
-// Date: October 13, 2021
-// Author: Shawn Hymel
-// License: 0BSD
 
-// Module: when buttons 1 and 2 are pressed, turn on LED
 module and_gate (
-
-    // Inputs
-    input   pmod_0,
-    input   pmod_1,
-    
-    // Outputs
-    output  led_0
+    input   I0, I1,
+    output  LED
 );
 
-    // Continuous assignment: NOT and AND operators
-    assign led_0 = ~pmod_0 & ~pmod_1;
+    assign LED = ~I0 & ~I1;       // Continuous assignment: NOT and AND operators
     
 endmodule
