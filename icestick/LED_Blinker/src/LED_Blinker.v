fcec3197dbf59eed7a0d4bfed5bd8fafc636f241
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-20 20:45:04 +0400

    wip

--- icestick/LED_Blinker/src/LED_Blinker.v
+++ icestick/LED_Blinker/src/LED_Blinker.v
@@ -7,20 +7,11 @@ module blink
 (
   input  CLK,
 
-  output LED1,
-  output LED2,
-  output LED3,
-  output LED4,
-  output LED5,
-
-  output Pmod1,
-  output Pmod2,
-  output Pmod3,
-  output Pmod4,
-  output Pmod7,
-  output Pmod8,
-  output Pmod9,
-  output Pmod10
+  output LED1, LED2, LED3, LED4, LED5,
+  output J1_3, J1_4, J1_5, J1_6, J1_7, J1_8, J1_9, J1_10,
+  output J2_1, J2_2, J2_3, J2_4, J2_7, J2_8, J2_9, J2_10,
+  output J3_3, J3_4, J3_5, J3_6, J3_7, J3_8, J3_9, J3_10
+  
 );
 
   localparam bits = 8;  // number of bits that you would like to toggle
@@ -35,6 +26,8 @@ module blink
   end
 
   assign { LED1, LED2, LED3, LED4, LED5 } = out;
-  assign { Pmod10, Pmod9, Pmod8, Pmod7, Pmod4, Pmod3, Pmod2, Pmod1 } = out;
-  
+  assign { J1_3, J1_4, J1_5, J1_6, J1_7, J1_8, J1_9, J1_10 } = out;
+  assign { J2_1, J2_2, J2_3, J2_4, J2_7, J2_8, J2_9, J2_10 } = out;
+  assign { J3_3, J3_4, J3_5, J3_6, J3_7, J3_8, J3_9, J3_10 } = out;
+
 endmodule
