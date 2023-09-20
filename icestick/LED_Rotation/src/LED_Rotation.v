62aae686ba1404090e215f0da00dff3c3d021bb5
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-20 20:58:18 +0400

    wup

--- icestick/LED_Rotation/src/LED_Rotation.v
+++ icestick/LED_Rotation/src/LED_Rotation.v
@@ -6,12 +6,7 @@
 module LED_Rotation(
     
 	input  clk,
-
-    output LED1,
-    output LED2,
-    output LED3,
-    output LED4,
-    output LED5
+    output LED1, LED2, LED3, LED4, LED5
     
 	);
 
