2b4e407aaaf5be690fb854d1fa91cec0a898ae0a
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-25 22:15:08 +0400

    wip

--- apio/icestick/clk_count/clk_count.v
+++ apio/icestick/clk_count/clk_count.v
@@ -16,23 +16,35 @@ module clock_counter
 
     assign rst = ~rst_btn;
 
-    always @ (posedge div_clk or posedge rst) begin
-        if (rst == 1'b1) begin
-            led <= 4'b0;
-        end else begin
-            led <= led + 1'b1;
+    // Counter
+    always @ (posedge div_clk or posedge rst) 
+        begin
+            if (rst == 1'b1)
+                begin
+                    led <= 4'b0;
+                end
+            else 
+                begin
+                    led <= led + 1'b1;
+                end
         end
-    end
-        
-    always @ (posedge clk or posedge rst) begin     // Clock divider
-        if (rst == 1'b1) begin
-            count <= 32'b0;
-        end else if (count == max_count) begin
-            count <= 32'b0;
-            div_clk <= ~div_clk;
-        end else begin
-            count <= count + 1;
+
+    // Clock divider    
+    always @ (posedge clk or posedge rst) 
+        begin
+            if (rst == 1'b1)
+                begin
+                    count <= 32'b0;
+                end 
+            else if (count == max_count) 
+                begin
+                    count <= 32'b0;
+                    div_clk <= ~div_clk;
+                end 
+            else 
+                begin
+                    count <= count + 1;
+                end
         end
-    end
     
 endmodule
