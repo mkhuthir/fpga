84f4fdcfe3ea10f77d77e73458ac38c0a87cacf8
Muthanna Alwahash <mkhuthir@gmail.com>
2023-09-25 21:38:42 +0400

    wip

--- apio/icestick/and_gate/and_gate.v
+++ apio/icestick/and_gate/and_gate.v
@@ -1,2 +1,14 @@
 // Muthanna Alwahash
 // (c) Sept 2023
+
+
+module and_gate 
+    (
+    input   pmod_0,
+    input   pmod_1,
+    output  led_0
+    );
+
+    assign led_0 = ~pmod_0 & ~pmod_1;
+
+endmodule
