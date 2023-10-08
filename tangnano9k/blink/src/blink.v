1bdcba2205f432a4ba725b23e1a46e1e6ead737e
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-08 11:30:04 +0400

    updated blink

--- tangnano9k/blink/src/blink.v
+++ tangnano9k/blink/src/blink.v
@@ -1,3 +1,6 @@
+// Muthanna Alwahash
+// October 2023
+
 module led (
     input sys_clk,          // clk input
     input sys_rst_n,        // reset input
@@ -8,20 +11,20 @@ reg [23:0] counter;
 
 always @(posedge sys_clk or negedge sys_rst_n) begin
     if (!sys_rst_n)
-        counter <= 24'd0;
-    else if (counter < 24'd1349_9999)       // 0.5s delay
+        counter <= 24'd0;                   // System reset
+    else if (counter < 24'd1349_9999)       // Before reaching 0.5s delay
         counter <= counter + 1'b1;
     else
-        counter <= 24'd0;
+        counter <= 24'd0;                   // Every 0.5s delay
 end
 
 always @(posedge sys_clk or negedge sys_rst_n) begin
     if (!sys_rst_n)
-        led <= 6'b111110;
-    else if (counter == 24'd1349_9999)       // 0.5s delay
+        led <= 6'b111110;                   // System Reset
+    else if (counter == 24'd1349_9999)      // When reaching 0.5s delay
         led[5:0] <= {led[4:0],led[5]};
     else
-        led <= led;
+        led <= led;                         // Before reaching 0.5s delay
 end
 
 endmodule
